e753d991b3281f0275556cafadbb075ab375001a
AKRA <99009898+AkraTimur@users.noreply.github.com>
2022-10-05 17:33:07 +0300

    RemoveAnchor done

---
+++ RemoveAnchor/RemoveAnchor/Kata.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveAnchor
+{
+    internal class Kata
+    {
+        public static string RemoveUrlAnchor(string url)
+        {
+            
+            string newUrl = "";
+            foreach (var item in url)
+            {
+                if (item != '#') newUrl = newUrl + item;
+                else break;
+            }
+           return newUrl;
+            
+
+        }
+    }
+}
---
+++ RemoveAnchor/RemoveAnchor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace RemoveAnchor
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.RemoveUrlAnchor("www.codewars.com#about"));
+            Console.WriteLine(Kata.RemoveUrlAnchor("www.codewars.com?page=1"));
+        }
+    }
+}
