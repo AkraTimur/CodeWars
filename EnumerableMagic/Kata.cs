693da03b73a13cd33219bf506463c15bd888b4b9
AKRA <99009898+AkraTimur@users.noreply.github.com>
2022-10-05 17:52:35 +0300

    done

---
+++ EnumerableMagic/Kata.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumerableMagic
+{
+    internal class Kata
+    {
+        public static bool Include(int[] arr, int item)
+        {
+            int res = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == item)
+                {
+                    res++;
+                }  
+            }
+            return res > 0;
+
+            // or  return arr/Comtains(item)
+           
+        }
+    }
+}
---
+++ EnumerableMagic/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace EnumerableMagic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.Include(new int[] { 1, 2, 3, 4 }, 25));
+        }
+    }
+}
