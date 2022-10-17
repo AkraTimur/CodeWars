61be0670e3229cb22fb046bb07a64eee28cc815f
AKRA <99009898+AkraTimur@users.noreply.github.com>
2022-10-17 18:09:15 +0300

    done

---
+++ Number of People in the Bus/Kata.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number_of_People_in_the_Bus
+{
+    internal class Kata
+    {
+        public static int Number(List<int[]> peopleListInOut)
+        {
+            int result = 0;
+            foreach (var item in peopleListInOut)
+            {
+               result += item[0];
+                result -= item[1];
+            }
+            return result;
+
+        }
+        
+    }
+}
---
+++ Number of People in the Bus/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Number_of_People_in_the_Bus
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int[]> peopleList = new List<int[]>() { new[] { 3, 0 }, 
+                new[] { 9, 1 }, new[] { 4, 10 }, new[] { 12, 2 }, 
+                new[] { 6, 1 }, new[] { 7, 10 } };
+
+            Console.WriteLine(Kata.Number(peopleList));
+        }
+    }
+}
