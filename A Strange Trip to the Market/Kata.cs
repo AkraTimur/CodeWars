35d7abbd09d230ba63e924fdf1337e6405f25ed5
AKRA <99009898+AkraTimur@users.noreply.github.com>
2022-10-17 11:57:56 +0300

    Strange trip done

---
+++ A Strange Trip to the Market/Kata.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_Strange_Trip_to_the_Market
+{
+    internal class Kata
+    {
+        public static bool IsLockNessMonster(string sentence)
+        {
+            sentence = sentence.ToLower();
+            string one = "tree fiddy";
+            string two = "3.50";
+            string three = "three fifty";
+            if ((sentence.Contains(one)) || (sentence.Contains(two)) || (sentence.Contains(three)))
+                return true;
+            else return false;
+        }
+    }
+}
---
+++ A Strange Trip to the Market/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace A_Strange_Trip_to_the_Market
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.IsLockNessMonster("Your girlscout cookies are ready to ship. " +
+                "Your total comes to tree fiddy"));
+        }
+    }
+}
