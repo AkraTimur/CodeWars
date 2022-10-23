665eded4a5b70a873b34f01ca3e12462f6e28883
AKRA <99009898+AkraTimur@users.noreply.github.com>
2022-10-23 11:59:17 +0300

    add new kata

---
+++ Printing Array elements with Comma delimiters/Kata.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Printing_Array_elements_with_Comma_delimiters
+{
+    public  class Kata
+    {
+        public static string PrintArray(object[] array)
+        {
+
+            return string.Join(",", array);
+        }
+    }
+}
---
+++ Printing Array elements with Comma delimiters/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Printing_Array_elements_with_Comma_delimiters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.PrintArray(new object[] { "hello,this,is,an,array!,a,b,c,d,e!" }));
+        }
+    }
+}
