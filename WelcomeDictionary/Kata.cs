4547fed051cf9fe423fad98a6fc7003127d41e2c
AKRA <99009898+AkraTimur@users.noreply.github.com>
2022-10-23 11:21:49 +0300

    add new Kata

---
+++ WelcomeDictionary/Kata.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WelcomeDictionary
+{
+    public  class Kata
+    {
+        public static string Greet(string language)
+        {
+            
+            Dictionary<string, string> ipAdress = new Dictionary<string, string>()
+            {
+                {"english", "Welcome"},
+                {"czech", "Vitejte"},
+                {"danish", "Velkomst"},
+                {"dutch", "Welkom"},
+                {"estonian", "Tere tulemast"},
+                {"finnish", "Tervetuloa"},
+                {"flemish", "Welgekomen"},
+                {"french", "Bienvenue"},
+                {"german", "Willkommen"},
+                {"irish", "Failte"},
+                {"italian", "Benvenuto"},
+                {"latvian", "Gaidits"},
+                {"lithuanian", "Laukiamas"},
+                {"polish", "Witamy"},
+                {"spanish", "Bienvenido"},
+                {"swedish", "Valkommen"},
+                {"welsh", "Croeso"}
+            };
+            if (ipAdress.TryGetValue(language, out var returnedValue))
+            {
+                return returnedValue;
+            }
+            else return "Welcome";
+        }
+    }
+}
---
+++ WelcomeDictionary/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace WelcomeDictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.Greet("english"));
+        }
+    }
+}
