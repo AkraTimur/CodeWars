ff070db17bee19194f38bf09148b3940c44042a1
AKRA <99009898+AkraTimur@users.noreply.github.com>
2022-10-23 15:33:33 +0300

    add new kata

---
+++ Remove First and Last Character Part Two/Kata.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Remove_First_and_Last_Character_Part_Two
+{
+    public  class Kata
+    {
+        public static string Array(string? s)
+        {
+            
+            if ((s==null) || (s.Length<=2))
+            {
+#pragma warning disable CS8603 // Possible null reference return.
+                return null;
+#pragma warning restore CS8603 // Possible null reference return.
+            }
+            else
+            {
+                return  s.Remove(0, 0 + 1)
+                       .Remove(s.Length - 2)
+                       .Replace(",", " ");
+            }
+            
+            
+        }
+    }
+}
+//string mystr = s.Remove(0, 0 + 1);
+//string mystr1 = s.Remove(s.Length - 1);
+//return mystr1;
---
+++ Remove First and Last Character Part Two/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Remove_First_and_Last_Character_Part_Two
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.Array("1"));
+        }
+    }
+}
