9d36e008f00a08f62df25cc211a0a73c25172b80
AKRA <99009898+AkraTimur@users.noreply.github.com>
2022-10-26 17:27:04 +0300

    add CreditCardMask

---
+++ Credit Card Mask/Kata.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Credit_Card_Mask
+{
+    public static class Kata
+    {
+        public static string Maskify(string cc)
+        {
+            if (cc.Length >= 4)
+            {
+                var c = cc.ToCharArray();
+                var symbol = '#';
+                for (int i = 0; i < cc.Length - 4; i++)
+                {
+                    c[i] = symbol;
+                }
+                return new string(c);
+            }else
+                return cc;
+
+        }
+    }
+}
---
+++ Credit Card Mask/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Credit_Card_Mask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.Maskify("aaaaaaasdsd"));
+        }
+    }
+}
