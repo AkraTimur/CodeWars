bd908305748f42df42dbcc26ddfa65620f6a6089
AKRA <99009898+AkraTimur@users.noreply.github.com>
2022-11-08 18:02:40 +0300

    add new kata

---
+++ Sum without highest and lowest number/Sum without highest and lowest number/Kata.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_without_highest_and_lowest_number
+{
+    public class Kata
+    {
+        public static int Sum(int[] numbers)
+        {
+            if ((numbers == null) || (numbers.Length <= 1))
+            {
+                return 0;
+            }
+            return numbers.Sum() - numbers.Min() - numbers.Max(); 
+        }
+    }
+}
