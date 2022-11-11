53831798878614262b8ce197972de2e81dc8c857
AKRA <99009898+AkraTimur@users.noreply.github.com>
2022-11-11 16:50:01 +0300

    add new Kata

---
+++ Mumbling/Mumbling/Kata.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mumbling
+{
+    internal class Kata
+    {
+        public static String Accum(string s)
+        {
+            return string.Join("-", s.Select((x, i) => char.ToUpper(x) + new string(char.ToLower(x), i)));
+
+        }
+    }
+}
