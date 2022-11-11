070e646b1372a4e311847045075cadcebb7243ab
AKRA <99009898+AkraTimur@users.noreply.github.com>
2022-11-11 12:23:48 +0300

    add new kata

---
+++ English/English/Kata.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace English
+{
+    public class Kata
+    {
+        public static bool SpeakEnglish(string sentence)
+        {
+            // Good luck!;
+            return sentence.ToLower().Contains("english");
+        }
+    }
+}
---
+++ English/English/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace English
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.SpeakEnglish("hello world"));
+        }
+    }
+}
+
+//Do you speak "English"?
+//    Given a string of arbitrary length with any ascii characters. Write a function to determine whether the string contains the whole word "English".
+
+//The order of characters is important -- a string "abcEnglishdef" is correct but "abcnEglishsef" is not correct.
+
+//Upper or lower case letter does not matter -- "eNglisH" is also correct.
+
+//Return value as boolean values, true for the string to contains "English", false for it does not.
