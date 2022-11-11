5305ef227b99a21190db05867b16c4f73746683f
AKRA <99009898+AkraTimur@users.noreply.github.com>
2022-11-11 15:56:19 +0300

    add new kata

---
+++ MaximumProduct/MaximumProduct/Kata.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaximumProduct
+{
+    public class Kata
+    {
+        public static int AdjacentElementsProduct(int[] array)
+        {
+            var list = array.ToList();
+            var myList = new List<int>();
+            int res = 0;
+            for (int i = 0; i < list.Count; i++)
+            {
+                for (int j = i + 1; j < list.Count; j++)
+                {
+                        res = array[j] * array[i];
+                        myList.Add(res);
+                    i++;
+                }
+            }
+            if (list[list.Count-1] == 0) return myList.Max();
+            if (res == 0) return res;
+            return myList.Max();
+        }
+
+    }
+}
+// return array.Skip(1).Select((x,i) => x * array[i]).Max();
---
+++ MaximumProduct/MaximumProduct/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace MaximumProduct
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           // Console.WriteLine(Kata.AdjacentElementsProduct(new int[]{ 9, 5, 10, 2, 24, -1, -48 }));
+            Console.WriteLine(Kata.AdjacentElementsProduct(new int[]{ 6, 1, 3, 1, 0}));
+        }
+    }
+}
+
+//Task
+//Given an array of integers , Find the maximum product obtained from multiplying 2 adjacent numbers in the array.
+
+//Notes
+//Array/list size is at least 2.
+
+//Array/list numbers could be a mixture of positives, negatives also zeroes .
+
+//Input >> Output Examples
+//adjacentElementsProduct([1, 2, 3]); ==> return 6
+//Explanation:
+//The maximum product obtained from multiplying 2 * 3 = 6, and they're adjacent numbers in the array.
+//adjacentElementsProduct([9, 5, 10, 2, 24, -1, -48]); ==> return 50
+//Explanation:
+//Max product obtained from multiplying 5 * 10  =  50 .
+
+
