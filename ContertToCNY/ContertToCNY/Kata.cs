834f50e10c8a1108ac04eba4f75c875975c07ab9
AKRA <99009898+AkraTimur@users.noreply.github.com>
2022-11-14 11:45:00 +0300

    add new kata

---
+++ ContertToCNY/ContertToCNY/Kata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContertToCNY
+{
+    internal class Kata
+    {
+        public static string Usdcny(int usd)
+        {
+            return (Convert.ToDecimal(usd) * 6.75M).ToString("0.00");
+        }
+    }
+}
