f0fc13b06ed92019db426775bd028df878a9da1a
AKRA <99009898+AkraTimur@users.noreply.github.com>
2022-11-14 11:24:27 +0300

    add new kata

---
+++ Apple Turnover/Apple Turnover/Kata.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apple_Turnover
+{
+    internal class Kata
+    {
+        public static string Apple(object n)
+        {
+          int a = int.Parse(n.ToString());
+            var squad = a * a;
+            if (squad > 1000)
+            {
+                return "It's hotter than the sun!!";
+            }
+            else return "Help yourself to a honeycomb Yorkie for the glovebox.";
+        }
+    }
+}
--- Mumbling/Mumbling/Kata.cs
+++ Mumbling/Mumbling/Kata.cs
@@ -12,7 +12,6 @@ namespace Mumbling
         public static String Accum(string s)
         {
             return string.Join("-", s.Select((x, i) => char.ToUpper(x) + new string(char.ToLower(x), i)));
-
         }
     }
 }
