664cacb0d00e351aaece9dc5e485f6929d91e646
AKRA <99009898+AkraTimur@users.noreply.github.com>
2022-11-16 15:09:32 +0300

    add new kata

---
+++ SleighAuthentication/SleighAuthentication/Sleigh.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SleighAuthentication
+{
+    public class Sleigh
+    {
+        public static bool Authenticate(string name, string password) => 
+            name.Contains("Santa Claus") && password.Contains("Ho Ho Ho!");
+    }
+}
