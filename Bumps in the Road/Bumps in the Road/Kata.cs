7377dafb98c33506dfb808a5b1e7bc8f6c400cf9
AKRA <99009898+AkraTimur@users.noreply.github.com>
2022-11-16 15:27:24 +0300

    add new kata

---
+++ Bumps in the Road/Bumps in the Road/Kata.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Bumps_in_the_Road
+{
+    public class Kata
+    {
+        public static string Bump(string input)
+        {
+            var pattern = "n";
+            Regex regex = new Regex(pattern);
+            var matches = regex.Matches(input);
+            if (matches.Count > 15) return "Car Dead";
+            else return "Woohoo!";
+        }
+    }
+}
