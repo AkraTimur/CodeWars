401b06be1e5fc091698bb3853bc9fd536524ab53
AKRA <99009898+AkraTimur@users.noreply.github.com>
2022-11-16 17:31:36 +0300

    add new kata

---
+++ Form The Minimum/Form The Minimum/Kata.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Form_The_Minimum
+{
+    public class Kata
+    {
+        public static long MinValue(int[] a)
+        {
+            Array.Sort(a);
+            string num = "";
+            for (int i = 0; i < a.Length; i++)
+            {
+                num = num + a[i];
+            }
+            var result = new string(num.Distinct().ToArray());
+            return Convert.ToInt64(result);
+            //Magic
+        }
+    }
+}
+
+//=> long.Parse(string.Concat(a.Distinct().OrderBy(x => x)));
---
+++ Form The Minimum/Form The Minimum/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Form_The_Minimum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Akra DEV");
+            Console.WriteLine(Kata.MinValue(new int[] { 1, 3, 1 }));
+        }
+    }
+}
+
+//Form The Minimum
+
+//    Task
+//Given a list of digits, return the smallest number that could be formed from these digits, using the digits only once(ignore duplicates).
+
+//Notes:
+//Only positive integers will be passed to the function (> 0 ), no negatives or zeros.
