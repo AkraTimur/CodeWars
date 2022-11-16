befb4c0fb8afa43ec20ff8e5206e75097034b621
AKRA <99009898+AkraTimur@users.noreply.github.com>
2022-11-16 14:51:29 +0300

    add new kata

--- FundamentalsReturn/FundamentalsReturn/Kata.cs
+++ FundamentalsReturn/FundamentalsReturn/Kata.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace FundamentalsReturn
 {
-    internal class Kata
+    public class Kata
     {
+        public static int Add(int a, int b) => a + b;
+        public static int Multiply(int a, int b) => a * b;
+        public static int Divide(int a, int b) => a / b;
+        public static int Mod(int a, int b) => a % b;
+        public static int Exponent(int a, int b) => (int)Math.Pow(a, b);
+        public static int Subt(int a, int b) => a - b;
     }
 }
---
+++ GrasshopperArrayMean/GrasshopperArrayMean/Kata.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrasshopperArrayMean
+{
+    public class Kata
+    {
+        public static int FindAverage(int[] nums) => nums.Sum() / nums.Length;
+    }
+}
+//return (int)nums.Average();
