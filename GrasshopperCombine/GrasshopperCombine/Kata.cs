661f8cd0b2657abe3847fe97c88e1f57421c8769
AKRA <99009898+AkraTimur@users.noreply.github.com>
2022-11-23 13:56:16 +0300

    add new kata

---
+++ GrasshopperCombine/GrasshopperCombine/Kata.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrasshopperCombine
+{
+    public class Kata
+    {
+        public static string CombineNames(string a, string b) => $"{a} {b}";
+    }
+}
+
+// alter: public static string CombineNames(params string[] names) => string.Join(" ", names);
