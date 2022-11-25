35cf56bdc85122bca64b33eec231d3dafd9990cc
AKRA <99009898+AkraTimur@users.noreply.github.com>
2022-11-25 11:04:09 +0300

    update

--- Factorial/Factorial/Kata.cs
+++ Factorial/Factorial/Kata.cs
@@ -10,7 +10,7 @@ namespace Factorial
     {
         public static int Factorial(int n)
         {
-            if (n < 0 && n > 12) throw new System.ArgumentOutOfRangeException();
+            if (n < 0 && n > 12) throw new ArgumentOutOfRangeException();
             if (n == 0) return 1;
             else return n * Factorial(n - 1);
         } 
