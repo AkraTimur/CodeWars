5763421ca5b1f484774269bc8aa0258e89902a4b
AKRA <99009898+AkraTimur@users.noreply.github.com>
2022-11-25 16:51:54 +0300

    add new kata

---
+++ FriendOrFoe/FriendOrFoe/Kata.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FriendOrFoe
+{
+    public class Kata
+    {
+        public static IEnumerable<string> FriendOrFoe(string[] names)
+        {
+            // Good luck!
+            var resList = new List<string>();
+            foreach (var name in names)
+            {
+                if(name.Length == 4) 
+                    resList.Add(name); 
+            }
+            return resList.AsEnumerable();
+        }
+    }
+}
+
+// return names.Where(name => name.Length == 4);
---
+++ FriendOrFoe/TestProject1/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace TestProject1
+{
+    using FriendOrFoe;
+    [TestFixture]
+    public class KataTests
+    {
+        [Test]
+        public void Test1()
+        {
+            string[] expected = { "Ryan", "Mark" };
+            string[] names = { "Ryan", "Kieran", "Mark", "Jimmy" };
+            CollectionAssert.AreEqual(expected, Kata.FriendOrFoe(names));
+        }
+    }
+}
