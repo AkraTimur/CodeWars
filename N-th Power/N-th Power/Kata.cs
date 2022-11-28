99e046048f175111f29717305bca4cae52d1a544
AKRA <99009898+AkraTimur@users.noreply.github.com>
2022-11-28 13:03:42 +0300

    add new kata

---
+++ N-th Power/N-th Power/Kata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace N_th_Power
+{
+    public static class Kata
+    {
+        public static double Index(int[] array, int n)
+        {
+            if (n > array.Length - 1) return (double)-1;
+            return Math.Pow((double)array[n], (double)n);
+        }
+    }
+}
+//  => n < array.Length ? Math.Pow(array[n],n) : -1;
