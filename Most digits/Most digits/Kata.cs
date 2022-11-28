0e25d92eb3a31a04e58fdcffaf2560e63853f858
AKRA <99009898+AkraTimur@users.noreply.github.com>
2022-11-28 14:59:51 +0300

    add new kata

---
+++ Most digits/Most digits/Kata.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Most_digits
+{
+    class Kata
+    {
+        public static int FindLongest(int[] number)
+        {
+            // code here
+            var list = new List<string>();
+
+            foreach (var item in number)
+            {
+                list.Add(Convert.ToString(item));
+            }
+
+            var listMax = list[0];
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i].Length > listMax.Length)
+                    listMax = list[i];
+            }
+
+            var res = "";
+            foreach (var item in list)
+            {
+                if(listMax.Length == item.Length)
+                {
+                    res = item;
+                    break;
+                }
+            }
+            return int.Parse(res);
+        }
+    }
+}
+//return N.OrderByDescending(X => X.ToString().Length).First();
