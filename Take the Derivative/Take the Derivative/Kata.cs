d0a42a1dd413bc9a2009a2965ecda29682237bc1
AKRA <99009898+AkraTimur@users.noreply.github.com>
2022-11-28 13:17:27 +0300

    add new kata

---
+++ Take the Derivative/Take the Derivative/Kata.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Take_the_Derivative
+{
+    public class Kata
+    {
+        public static string Derive(double coefficient, double exponent) => $"{coefficient * exponent}x^{exponent - 1}";
+    }
+}
