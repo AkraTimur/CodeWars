62c7bdbef8255e1c41e53aa8dda4b0e046993d04
AKRA <99009898+AkraTimur@users.noreply.github.com>
2022-11-28 14:11:45 +0300

    add new kata

---
+++ Get number from string/Get number from string/Kata.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Get_number_from_string
+{
+    class Kata
+    {
+        public static int getNumberFromString(string s) => Convert.ToInt32(string.Join("", s.ToCharArray().Where(Char.IsDigit)));
+    }
+}
+ 
---
+++ Get number from string/Get number from string/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Get_number_from_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+//Get number from string
+//    Write a function which removes from string all non-digit characters and parse the remaining to number. E.g: "hell5o wor6ld"-> 56
+
