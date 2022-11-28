39a473cc48d6a87506a34d1c7fab719cf84149b3
AKRA <99009898+AkraTimur@users.noreply.github.com>
2022-11-28 12:48:39 +0300

    add new kata

---
+++ validate code with simple regex/validate code with simple regex/Kata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace validate_code_with_simple_regex
+{
+    public static class Kata
+    {
+        public static bool ValidateCode(string code)
+        {
+            //use regex here
+            return Regex.IsMatch(code, "^[1-3]");
+        }
+    }
+}
---
+++ validate code with simple regex/validate code with simple regex/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace validate_code_with_simple_regex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.ValidateCode("123"));
+        }
+    }
+}
