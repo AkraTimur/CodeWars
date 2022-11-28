ccfe7d5df23558e18adf2b255e1909518de15b07
AKRA <99009898+AkraTimur@users.noreply.github.com>
2022-11-28 13:28:44 +0300

    add new kata

---
+++ Is it a number/Is it a number/Kata.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Is_it_a_number
+{
+    public class Kata
+    {
+        public static bool IsDigit(string s) => double.TryParse(s, out _);
+    }
+}
---
+++ Is it a number/Is it a number/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Is_it_a_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.IsDigit("3"));
+        }
+    }
+}
+//Is it a number
+//    Given a string s, write a method (function) that will return true if its a valid single integer or floating number or false if its not.
+
+//Valid examples, should return true:
+
+//isDigit("3")
+//isDigit("  3  ")
+//isDigit("-3.23")
+//should return false:
+
+//isDigit("3-4")
+//isDigit("  3   5")
+//isDigit("3 5")
+//isDigit("zero")
