28f1066b3479bd7067bf54ae9e633e30a3c14923
AKRA <99009898+AkraTimur@users.noreply.github.com>
2022-11-29 12:50:16 +0300

    add new kata

---
+++ codewars_7_kye/RegexValidate/Kata.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace RegexValidate
+{
+    public class Kata
+    {
+        public static bool ValidatePin(string pin) => Regex.IsMatch(pin, @"^[0-9]{4}$|^[0-9]{6}$");
+    }
+}
---
+++ codewars_7_kye/RegexValidate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RegexValidate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.ValidatePin("1234"));
+        }
+    }
+}
