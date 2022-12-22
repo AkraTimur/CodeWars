c69dda3b7425a944ab1ff9f4b5e8308edc595607
AKRA <timur.akra@outlook.com>
2022-12-22 21:59:18 +0300

    add new Kata AddLength

---
+++ AddLength/Kata.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddLength
+{
+    public class Kata
+    {
+        public static string[] AddLength(string str)
+        {
+            var array = str.Split();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = $"{array[i]} {array[i].Length}";
+            }
+            return array;
+        }
+    }
+}
+//=> str.Split(' ').Select(e=> $"{e} {e.Length}").ToArray();
---
+++ AddLength/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace AddLength
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            var array = Kata.AddLength("apple ban");
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
+//Add Length
+//What if we need the length of the words separated by a space to be added at the end of that same word and have it returned as an array?
+
+//Example(Input --> Output)
+
+//"apple ban"-- > ["apple 5", "ban 3"]
+//"you will win"-- >["you 3", "will 4", "win 3"]
+//Your task is to write a function that takes a String and returns an Array/list with the length of each word added to each element .
+
+//Note: String will have at least one element; words will always be separated by a space.
