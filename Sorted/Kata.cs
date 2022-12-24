e3e7b59776e3242ca51bb8deeedf8e790201ff6a
AKRA <99009898+AkraTimur@users.noreply.github.com>
2022-12-24 15:49:55 +0300

    add new kata Sorted

---
+++ Sorted/Kata.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.XPath;
+
+namespace Sorted
+{
+    internal class Kata
+    {
+        
+        public static string IsSortedAndHow(int[] array)
+        {
+            var list = array.ToList();
+            var list2 = list.OrderBy(x => x).ToList();
+            var list3 = list.OrderByDescending(x => x).ToList();
+
+            if (list.SequenceEqual(list2))
+            {
+                return "yes, ascending";
+            }
+            else if (list.SequenceEqual(list3)) return "yes, descending";
+            
+            return "no";
+        }
+    }
+}
---
+++ Sorted/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Sorted
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.IsSortedAndHow(new[] { 1, 2 }));
+            Console.WriteLine(Kata.IsSortedAndHow(new[] { 4, 2, 30 }));
+            
+        }
+    }
+}
+
+//Sorted? yes? no? how?
+//    Complete the method which accepts an array of integers, and returns one of the following:
+
+//"yes, ascending" - if the numbers in the array are sorted in an ascending order
+//"yes, descending" - if the numbers in the array are sorted in a descending order
+//"no" - otherwise
+//You can assume the array will always be valid, and there will always be one correct answer.
