1ab96334025ac521e14b36035f7e63aa787bc2e8
AKRA <99009898+AkraTimur@users.noreply.github.com>
2022-12-24 15:19:53 +0300

    add new kata Switcheroo

---
+++ Switcheroo/Kata.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switcheroo
+{
+    internal class Kata
+    {
+        public static string Switcheroo(string x)
+        {
+           var array = x.ToCharArray();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == 'a')
+                {
+                    array[i] = 'b';
+
+                }
+               else if (array[i] == 'b') array[i] = 'a';
+            }
+            return string.Join("", array);
+        }
+    }
+}
---
+++ Switcheroo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Switcheroo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.Switcheroo("abc"));
+        }
+    }
+}
