53d383181c0c905025c41891fb1dfa8fbf6c1b4c
AKRA <99009898+AkraTimur@users.noreply.github.com>
2022-12-27 18:14:30 +0300

    add new kata Contamination

---
+++ Contamination/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Contamination
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.Contamination("abc", "z"));
+        }
+    }
+}
+//Contamination #1 -String-
+//    An AI has infected a text with a character!!
+
+//This text is now fully mutated to this character.
+
+//If the text or the character are empty, return an empty string.
+//There will never be a case when both are empty as nothing is going on!!
+
+//Note: The character is a string of length 1 or an empty string.
