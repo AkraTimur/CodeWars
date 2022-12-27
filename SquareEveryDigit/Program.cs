60daabd888a1574bc4dc3c6305600ed4b368a434
AKRA <99009898+AkraTimur@users.noreply.github.com>
2022-12-27 19:12:13 +0300

    add new kata SquareEveryDigit

---
+++ SquareEveryDigit/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SquareEveryDigit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.SquareDigits(9119));
+        }
+    }
+}
