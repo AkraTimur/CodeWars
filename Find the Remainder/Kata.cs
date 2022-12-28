1be0ed8a36c98503d439690211f33dd199e45ce1
AKRA <99009898+AkraTimur@users.noreply.github.com>
2022-12-28 10:54:50 +0300

    add new kata Find_the_Remainder

---
+++ Find the Remainder/Kata.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Find_the_Remainder
+{
+    public class Kata
+    {
+        public static int Remainder(int a, int b)
+        {
+            int number = 0;
+            int divaider = 0;
+            if(a > b)
+            {
+                number = a;
+                divaider = b;
+            }
+            else
+            {
+                number = b;
+                divaider = a;
+            }
+            if (divaider == 0) throw new DivideByZeroException();
+            return number % divaider;
+
+        }
+    }
+}
