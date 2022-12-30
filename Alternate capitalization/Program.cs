8b6446740140518a83864da91dcf9c69b4698248
AKRA <99009898+AkraTimur@users.noreply.github.com>
2022-12-30 11:38:36 +0300

    add new kata Alternate_capitalization

---
+++ Alternate capitalization/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.Intrinsics.X86;
+
+namespace Alternate_capitalization
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.Capitalize("abcdef"));
+        }
+    }
+}
+// Alternate capitalization
+//Given a string, capitalize the letters that occupy even indexes and odd indexes separately, and return as shown below. Index 0 will be considered even.
+
+//For example, capitalize("abcdef") = ['AbCdEf', 'aBcDeF']. See test cases for more examples.
+
+//The input will be a lowercase string with no spaces.
