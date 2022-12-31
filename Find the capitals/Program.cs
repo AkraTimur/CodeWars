09ba0e5280a83106bda314d0b5fb2d1df36316ee
AKRA <timur.akra@outlook.com>
2022-12-31 19:52:55 +0300

    add new kata Find_the_capitals

---
+++ Find the capitals/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Find_the_capitals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+//Find the capitals
+//Write a function that takes a single string (word) as argument.The function must return an ordered list containing the indexes of all capital letters in the string.
