14ef7633ec4cf85f6f2138e51a3b02fafb2ba230
AKRA <timur.akra@outlook.com>
2023-01-01 21:07:24 +0300

    add new kata Convert_an_array_of_strings_to_array_of_numbers

---
+++ Convert an array of strings to array of numbers/stringArrayMethods.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convert_an_array_of_strings_to_array_of_numbers
+{
+    public class stringArrayMethods
+    {
+        public static double[] ToDoubleArray(string[] stringArray)
+        {
+            var list = new List<double>();
+            foreach (var item in stringArray) 
+            {
+                list.Add(double.Parse(item,CultureInfo.InvariantCulture.NumberFormat));
+            }
+            return list.ToArray();
+        }
+    }
+}
