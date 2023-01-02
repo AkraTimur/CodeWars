a1282be7cdf4659f16d1e5733017239d5b13a2f0
AKRA <timur.akra@outlook.com>
2023-01-02 16:43:22 +0300

    add new kata Love_vs_friendship

---
+++ Love vs friendship/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Love_vs_friendship
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.WordsToMarks("attitude"));
+        }
+    }
+}
+//Love vs friendship
+
+//If a = 1, b = 2, c = 3... z = 26
+
+//Then l + o + v + e = 54
+
+//and f + r + i + e + n + d + s + h + i + p = 108
+
+//So friendship is twice as strong as love :-)
+
+//Your task is to write a function which calculates the value of a word based off the sum of the alphabet positions of its characters.
+
+//The input will always be made of only lowercase letters and will never be empty.
