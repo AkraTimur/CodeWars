be7126dc30f6cd223ebb51caa35055d688a646cb
AKRA <timur.akra@outlook.com>
2023-01-02 10:50:11 +0300

    add new kata Removing_Elements

---
+++ Removing Elements/Removing Elements/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Removing_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var res = Kata.RemoveEveryOther(new object[] { "Hello", "Goodbye", "Hello Again" });
+            foreach (var item in res)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
