0eca5428492aefb140d78b20d1ad5271d156475e
AKRA <timur.akra@outlook.com>
2023-01-02 16:15:54 +0300

    add new kata Enumerable_Magic_25___Take_the_First_N_Elements

---
+++ Enumerable Magic 25 - Take the First N Elements/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Enumerable_Magic_25___Take_the_First_N_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = Kata.Take(new int[] { }, 3);
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
+//Enumerable Magic #25 - Take the First N Elements
+//Create a function that accepts a list/array and a number n, and returns a list/array of the first n elements from the 
+//list/array.
