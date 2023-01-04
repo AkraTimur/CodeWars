a436ad07a92b62515b49c8b1945ad78708cdc88c
AKRA <timur.akra@outlook.com>
2023-01-04 07:40:23 +0300

    add new kata Build_Tower

---
+++ Build Tower/Build Tower/Kata.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Build_Tower
+{
+    public class Kata
+    {
+        public static string[] TowerBuilder(int nFloors)
+        {
+            var result = new string[nFloors];
+
+            // этажи
+            for (int i = 0; i < nFloors; i++)
+            {
+                result[i] = string.Concat(new string(' ', nFloors - i - 1),
+                                new string('*', i * 2 + 1),
+                                new string(' ', nFloors - i - 1));
+            }
+            return result;
+        }
+    }
+}
---
+++ Build Tower/Build Tower/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Build_Tower
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = Kata.TowerBuilder(3);
+
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
