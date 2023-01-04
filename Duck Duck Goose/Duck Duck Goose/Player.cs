227093a797e422af811681fd048d25247a766495
AKRA <timur.akra@outlook.com>
2023-01-04 08:12:08 +0300

    add new kata Duck_Duck_Goose

---
+++ Duck Duck Goose/Duck Duck Goose/Player.cs
@@ -0,0 +1,12 @@
+﻿namespace Duck_Duck_Goose
+{
+    public class Player
+    {
+        public string Name { get; set; }
+
+        public Player(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
