6747b3ce95b3e4ba5dccdf223a75d96a91f56aa4
AKRA <timur.akra@outlook.com>
2023-01-05 18:29:27 +0300

    add new kata

---
+++ get ascii value of character/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace get_ascii_value_of_character
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetASCII('A'));
+        }
+        public static int GetASCII(char c)
+        {
+            return (int)c;
+        }
+    }
+}
