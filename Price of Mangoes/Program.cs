425a409f1e7d5d237334defe441da632a5781873
AKRA <timur.akra@outlook.com>
2023-01-06 16:54:55 +0300

    add new kata

---
+++ Price of Mangoes/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Price_of_Mangoes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Mango(3,3));
+        }
+        public static int Mango(int quantity, int price)
+        {
+            return quantity * price - (quantity / 3 * price);
+        }
+    }
+}
