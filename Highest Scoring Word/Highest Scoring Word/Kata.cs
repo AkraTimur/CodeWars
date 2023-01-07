dd231d02f0ff1987c5303df24d841f4d479bda7e
AKRA <timur.akra@outlook.com>
2023-01-07 11:31:48 +0300

    add new kata Highest_Scoring_Word

---
+++ Highest Scoring Word/Highest Scoring Word/Kata.cs
@@ -0,0 +1,38 @@
+﻿namespace Highest_Scoring_Word
+{
+
+    public class HightWord
+    {
+        public  string WordName { get; set; }
+        public  int WordValue { get; set; }
+
+        public  HightWord(string name, int count) 
+        {
+            WordName= name;
+            WordValue = count;    
+        }
+
+    }
+    public class Kata
+    {
+        public static string High(string s)
+        {
+            var list = new List<HightWord>();
+            int count = 0;
+            var arr = s.Trim()
+                .Split();
+            foreach (var word in arr)
+            {
+                count = 0;
+                foreach (var item in word)
+                {
+                    count = count + (item % 32);
+                }
+                list.Add(new HightWord(word, count));
+            }
+
+            return list.OrderByDescending(x => x.WordValue).Select(x => x.WordName).First(); 
+        }
+    }
+}
+//s.Split(' ').OrderByDescending(x => x.Sum(z => z - 'a' + 1)).First();
