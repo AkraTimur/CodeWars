b920490a06dd436a2fe0a805a23508b753715d7c
AKRA <timur.akra@outlook.com>
2023-01-08 08:58:25 +0300

    add new kata

---
+++ How old will I be in 2099/AgeDiff.cs
@@ -0,0 +1,22 @@
+﻿namespace How_old_will_I_be_in_2099
+{
+    public class AgeDiff
+    {
+        public static string CalculateAge(int birth, int yearTo)
+        {
+            var res = yearTo - birth;
+
+            if (birth == yearTo) return "You were born this very year!";
+
+            else if (res == 1) return $"You are {res} year old.";
+
+            else if (res > 1) return $"You are {res} years old.";
+
+            else if (res == -1) return $"You will be born in {res * -1} year.";
+
+
+            return $"You will be born in {res * -1} years."; 
+
+        }
+    }
+}
