65aad4c37f6cbab5694a624ed1e99bd8f04b0d15
AKRA <timur.akra@outlook.com>
2023-01-08 13:38:51 +0300

    add new kata

---
+++ Pillars/Pillars/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Pillars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.Pillars(2, 20, 25));
+            Console.WriteLine(Kata.Pillars(11, 15, 30));
+        }
+    }
+}
