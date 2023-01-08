7e538a99f5f81fc7ec862219e50efdaacf81ff73
AKRA <timur.akra@outlook.com>
2023-01-08 12:40:59 +0300

    update

--- Largest pair sum in array/Largest pair sum in array/Kata.cs
+++ Largest pair sum in array/Largest pair sum in array/Kata.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Largest_pair_sum_in_array
+﻿namespace Largest_pair_sum_in_array
 {
     public class Kata
     {
