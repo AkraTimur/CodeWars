90e3a33b2245b85c4207f8cef052215590d3dc6b
AKRA <timur.akra@outlook.com>
2023-01-08 18:27:33 +0300

    add new kata

---
+++ Check the exam/Check the exam/Kata.cs
@@ -0,0 +1,25 @@
+﻿namespace Check_the_exam
+{
+    public class Kata
+    {
+        public static int CheckExam(string[] arr1, string[] arr2)
+        {
+            int count = 0;
+            int i = 0;
+
+            foreach (var item1 in arr1)
+            {
+                for (; i < arr2.Length;)
+                {
+                    if (item1 == arr2[i]) count += 4;
+                    else if (item1 != arr2[i] && arr2[i] != "") count -= 1;
+                    else count += 0;
+                    break;
+                }
+                i++;
+            }
+            if (count < 0) return 0;
+            return count;
+        }
+    }
+}
---
+++ Check the exam/Check the exam/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Check_the_exam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.CheckExam(new string[] { "a", "a", "c", "b" }, new string[] { "a", "a", "b", "" }));
+        }
+    }
+}
