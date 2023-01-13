3532f937dd4e7d7de709398abee4614c077840bd
AKRA <99009898+AkraTimur@users.noreply.github.com>
2023-01-13 14:15:09 +0300

    add new kata

---
+++ Build a pile of Cubes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Build_a_pile_of_Cubes
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public static long findNb(long m)
+        {
+            long sum = 0;
+            int n = 0;
+            while (sum < m)
+            {
+                n++;
+                sum += (long)Math.Pow(n, 3);
+            }
+            if (sum == m)
+                return n;
+            else
+                return -1;
+        }
+    }
+    
+}
