4afc1bb274d223d6a3bfa882a1837c7668b82ee1
AKRA <timur.akra@outlook.com>
2023-01-13 22:25:46 +0300

    add new kata

---
+++ Sort Out The Men From Boys/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Sort_Out_The_Men_From_Boys
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = Solution.MenFromBoys(new int[] { 7, 3, 14, 17 });
+
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+            
+        }
+    }
+}
