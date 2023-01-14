c914f5fad0f91ae8282ffcfa6c018c8b9bc07d1c
AKRA <99009898+AkraTimur@users.noreply.github.com>
2023-01-14 11:53:24 +0300

    add new kata

---
+++ Divide and Conquer/Kata.cs
@@ -0,0 +1,27 @@
+﻿namespace Divide_and_Conquer
+{
+    public class Kata
+    {
+        public static int DivCon(Object[] objArray)
+        {
+            int sumOfInt = 0;
+            int sumOfString = 0;
+
+            foreach (Object obj in objArray) 
+            {
+                if (obj.GetType() == typeof(int))
+                {
+                    var num = (int)obj;
+                    sumOfInt += num;
+                }
+                else if (obj.GetType() == typeof(string))
+                {
+                    var numStr = obj.ToString();
+                    sumOfString += Convert.ToInt32(numStr);
+                }
+            
+            }
+            return sumOfInt - sumOfString;
+        }
+    }
+}
---
+++ Divide and Conquer/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Divide_and_Conquer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.DivCon(new object[] { 9, 3, "7", "3" }));
+        }
+    }
+}
