31e6be1cbb3ab89259376b2e1b012f2998c1617f
AKRA <timur.akra@outlook.com>
2023-01-15 08:41:10 +0300

    add new kata

---
+++ Find the vowels/Kata.cs
@@ -0,0 +1,21 @@
+﻿namespace Find_the_vowels
+{
+    public class Kata
+    {
+        public static int[] VowelIndices(string word)
+        {
+            var indices = new List<int>();
+            for (int i = 0; i < word.Length; i++)
+            {
+                word = word.ToLower();
+                if (word[i] == 'a' || word[i] == 'e' || word[i] == 'i' || 
+                    word[i] == 'o' || word[i] == 'u' || word[i] == 'y')
+                {
+                    indices.Add(i+1);
+                }
+            }
+            return indices.ToArray();
+        }
+    }
+}
+//return Regex.Matches(word, "(?i)[aeiouy]").Select(m => m.Index + 1).ToArray();
