7b72907c85cde0c2bb58b7f19fab6149747e5c89
AKRA <timur.akra@outlook.com>
2023-01-20 08:09:27 +0300

    add new kata

---
+++ Incrementer/Kata.cs
@@ -0,0 +1,17 @@
+﻿namespace Incrementer
+{
+    public class Kata
+    {
+        public static int[] Incrementer(int[] numbers)
+        {
+            if (numbers.Length < 1) return new int[0];
+            int index = 1;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = numbers[i] + (index + i);
+                if (numbers[i] >= 10) numbers[i] = numbers[i]%10;
+            }
+            return numbers; 
+        }
+    }
+}
