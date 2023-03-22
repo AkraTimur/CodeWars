c05905e79e3b18cc7ceab7f84859bf1548809218
AKRA <AkraTimur@users.noreply.github.com>
2023-03-22 09:02:34 +0300

    update kata

---
+++ Valid Parentheses/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Valid_Parentheses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ValidParentheses("()"));
+        }
+        public static bool ValidParentheses(string str)
+        {
+            var stack = new Stack<char>();
+
+            foreach(char c in str)
+            {
+                if(c == '(')
+                {
+                    stack.Push(c);
+                }
+
+                else if (c == ')')
+                {
+                    if(stack.Count == 0 || stack.Peek() != '(')
+                    {
+                        return false;
+                    }
+                    stack.Pop();
+                }
+            }
+            return stack.Count == 0;
+        }
+    }
+}
