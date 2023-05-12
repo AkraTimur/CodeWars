7befd08a87a1c007c00659d40eb4b44919b31d3d
AKRA <AkraTimur@users.noreply.github.com>
2023-05-12 09:33:45 +0300

    add new kata

---
+++ Categorize New Member/Classes/Kata.cs
@@ -0,0 +1,42 @@
+﻿namespace Categorize_New_Member.Classes
+{
+
+    public class Kata
+    {
+        public static IEnumerable<string> OpenOrSenior(int[][] data)
+        {
+            var strings = new List<string>();
+
+            foreach (var item in data)
+            {
+               var age = item[0];
+
+
+                var handicap = item[1];
+                if (handicap > -2 && handicap < 26)
+                {
+                    handicap = age - handicap;
+                }
+                else
+                {
+                    handicap = 0;
+                }
+
+               var status = "";
+
+               if (age >= 55 && handicap > 7)
+               {
+                    status = "Senior";
+               }
+               else
+               {
+                   status = "Open";
+               }
+
+                strings.Add(status);
+            }
+
+            return strings;
+        }
+    }
+}
---
+++ Categorize New Member/Program.cs
@@ -0,0 +1,22 @@
+﻿using Categorize_New_Member.Classes;
+
+namespace Categorize_New_Member
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Print(Kata.OpenOrSenior(new[] { new[] { 45, 12 }, new[] { 55, 21 }, new[] { 19, 2 }, new[] { 104, 20 } }));
+            Print(Kata.OpenOrSenior(new[] { new[] { 3, 12 }, new[] { 55, 1 }, new[] { 91, -2 }, new[] { 54, 23 } }));
+            //Print(Kata.OpenOrSenior(new[] { new[] { 59, 12 }, new[] { 45, 21 }, new[] { -12, -2 }, new[] { 12, 12 } }));
+        }
+
+        public static void Print(IEnumerable<string> strings)
+        {
+            foreach (var item in strings)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
