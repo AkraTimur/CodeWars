5f432471cefe0aca1e8ebb29f2715e216c46d7ce
AKRA <timur.akra@outlook.com>
2023-05-26 09:42:56 +0300

    add new kata

---
+++ Cats in hats/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Cats_in_hats
+{
+    internal class Program
+    {
+
+        public static string Height(int n)
+        {
+            decimal oneCat = 2000000;
+            decimal totalHeight = oneCat;
+
+            for (int i = 1; i <= n; i++)
+            {
+                oneCat /= 2.5m;
+                totalHeight += oneCat;
+            }
+
+            return totalHeight.ToString("F3");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Height(7));
+        }
+    }
+}
