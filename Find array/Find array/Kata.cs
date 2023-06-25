3422798b4ea1a657818acae9f53d3b52ac436875
AKRA <AkraTimur@users.noreply.github.com>
2023-06-25 06:36:45 +0300

    add new kata

---
+++ Find array/Find array/Kata.cs
@@ -0,0 +1,23 @@
+﻿namespace Find_array_cs
+{
+    public class Kata
+    {
+        public static object[] FindArray(object[] arr1, int[] arr2)
+        {
+            var list = new List<object>();
+            var exitList = new List<object>();
+
+            if (arr1.Length <= 0 || arr2.Length <= 0) return exitList.ToArray();
+
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                if (arr2[i] > arr1.Length) return exitList.ToArray();
+
+                var obj = arr1[arr2[i]];
+                list.Add(obj);
+            }
+
+            return list.ToArray();
+        }
+    }
+}
---
+++ Find array/TestProject/Usings.cs
@@ -0,0 +1,2 @@
+global using NUnit.Framework;
+global using Find_array_cs;
