4831ce4ebe7c0e7616f6e82632e8c2b8fd5e645c
AKRA <AkraTimur@users.noreply.github.com>
2023-08-09 18:13:02 +0300

    add new kata

---
+++ Min Factor Distance/Min Factor Distance/Kata.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Min_Factor_Distance
+{
+    public static class Kata
+    {
+        public static int MinDistance(int n)
+        {
+            var array = new List<int>();
+
+            for (int count = 1; count < n+1; count++)
+            {
+                if(n % count == 0)
+                {
+                    array.Add(count);
+                }
+            }
+
+            var minDifference = array.SelectMany((a, i) => array.Skip(i + 1), (a, b) => Math.Abs(a - b)).Min();
+
+            return minDifference;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.MinDistance(13013));
+        }
+    }
+}
