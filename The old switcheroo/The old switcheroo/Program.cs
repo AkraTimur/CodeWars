b5ef3450577bbd5fc6095ad09b9719963d2d8bcf
AKRA <AkraTimur@users.noreply.github.com>
2023-08-11 08:45:08 +0300

    add new kata The_old_switcheroo

---
+++ The old switcheroo/The old switcheroo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace The_old_switcheroo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
