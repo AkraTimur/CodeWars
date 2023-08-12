e7b88c268e0607165fc5ae8df96c714850b82b7e
AKRA <AkraTimur@users.noreply.github.com>
2023-08-12 08:24:17 +0300

    add new kata Jungerstein_s_Math_Training_Room

---
+++ Jungerstein's Math Training Room/Jungerstein's Math Training Room/Kata.cs
@@ -0,0 +1,45 @@
+﻿using System.Numerics;
+
+namespace Jungerstein_s_Math_Training_Room
+{
+    public static class Kata
+    {
+        public static int CountZeros(int n)
+        {
+            BigInteger mult = 1;
+            if (n % 2 == 0)
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    if(i % 2 == 0)
+                    {
+                        mult *= i;
+                    }
+                }
+            }
+            else
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    if (j % 2 != 0)
+                    {
+                        mult *= j;
+                    }
+                }
+            }
+
+            var res = mult.ToString();
+            var zeroCount = 0;
+            for (int count = res.Length-1; count >= 0; count--)
+            {
+                if (res[count].ToString() == "0")
+                {
+                    zeroCount++;
+                }
+                else return zeroCount;
+
+            }
+            return zeroCount;
+        }
+    }
+}
