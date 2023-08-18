83c5c898dddcad256e714bc4a0a23a356ba7c441
AKRA <AkraTimur@users.noreply.github.com>
2023-08-18 15:48:55 +0300

    add new kata

---
+++ Array element parity/Array element parity/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Array_element_parity
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public static class Kata
+    {
+        public static int Solve(List<int> arr)
+        {
+            // Используем LINQ для нахождения элемента, который не имеет пары с противоположным знаком
+            return arr.FirstOrDefault(x => !arr.Contains(-x));
+        }
+    }
+}
---
+++ Array element parity/Array element parityTests/KataTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using Array_element_parity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_element_parity.Tests
+{
+    [TestFixture()]
+    public class KataTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.That(Kata.Solve(new List<int> { 1, -1, 2, -2, 3 }), Is.EqualTo(3));
+            Assert.That(Kata.Solve(new List<int> { -3, 1, 2, 3, -1, -4, -2 }), Is.EqualTo(-4));
+            Assert.That(Kata.Solve(new List<int> { 1, -1, 2, -2, 3, 3 }), Is.EqualTo(3));
+            Assert.That(Kata.Solve(new List<int> { -110, 110, -38, -38, -62, 62, -38, -38, -38 }), Is.EqualTo(-38));
+            Assert.That(Kata.Solve(new List<int> { -9, -105, -9, -9, -9, -9, 105 }), Is.EqualTo(-9));
+        }
+    }
+}
