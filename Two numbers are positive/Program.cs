c43663b32267f17d81b63f9b779b5fc3b56c7426
AKRA <AkraTimur@users.noreply.github.com>
2023-08-18 09:29:41 +0300

    add new kata

---
+++ Two numbers are positive/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Two_numbers_are_positive
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+    public static class Kata
+    {
+        public static bool TwoArePositive(int a, int b, int c)
+        {
+            var list = new List<int> 
+            {
+                a, b, c
+            };
+            int flag = 0;
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i]> 0)
+                {
+                    flag++;
+                }
+            }
+
+            return flag == 2;
+        }
+    }
+}
